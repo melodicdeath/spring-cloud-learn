56b02cd1c0574abb40c057404c45ad07eb9c5a95
zt.melody <zt.freedom@gmail.com>
2019-09-02 12:39:55 +0800

    config test

--- microservice-config-client/src/main/java/com/heynemo/microserviceconfigclient/controller/ConfigClientController.java
+++ microservice-config-client/src/main/java/com/heynemo/microserviceconfigclient/controller/ConfigClientController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConfigClientController {
-    @Value("${profile}")
+    @Value("${info.test}")
     private String profile;
 
     @GetMapping("/profile")
